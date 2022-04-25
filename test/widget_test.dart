1bc8ba4bce61e60265c3b892990b746b4363bb54
radovanovicslobodan <r.slobodan75@gmail.com>
2022-04-25 13:00:12 +0200

    remove blank line

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:multipage/email_widget.dart';
-
 import 'package:multipage/password_widget.dart';
 
 void main() {
